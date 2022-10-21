5cf36216d2110c2731ad75a92a9131294c17483f
victor-vila <victor.vila.navazo@gmail.com>
2022-10-21 15:44:53 +0200

    Spark local cluster in progress

---
+++ deploy/dev/spark/build.sh
@@ -0,0 +1,31 @@
+# -- Software Stack Version
+
+SPARK_VERSION="3.3.0"
+HADOOP_VERSION="3"
+JUPYTERLAB_VERSION="3.4.8"
+
+# -- Building the Images
+
+docker build \
+  -f spark_cluster/docker/cluster-base.Dockerfile \
+  -t cluster-base .
+
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f spark_cluster/docker/spark-base.Dockerfile \
+  -t spark-base .
+
+docker build \
+  -f spark_cluster/docker/spark-master.Dockerfile \
+  -t spark-master .
+
+docker build \
+  -f spark_cluster/docker/spark-worker.Dockerfile \
+  -t spark-worker .
+
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
+  -f spark_cluster/docker/jupyterlab.Dockerfile \
+  -t jupyterlab .
---
+++ deploy/dev/spark/spark_cluster/utils/spark_cluster_context_config.py
@@ -0,0 +1,7 @@
+spark_cluster_context_config = {
+    "spark.submit.deployMode": "client",
+    "spark.driver.bindAddress": "0.0.0.0",
+    "spark.ui.showConsoleProgress": "true",
+    "spark.eventLog.enabled": "false",
+    "spark.executor.memory": "2GB",
+}
