da69832212ae25c98f37c2f027089e6a96170381
victor-vila <victor.vila.navazo@gmail.com>
2022-10-25 17:10:58 +0200

    cleaning and black formatting

--- deploy/dev/spark/spark_cluster/examples/pyspark_example_1.py
+++ deploy/dev/spark/spark_cluster/examples/pyspark_example_1.py
@@ -6,11 +6,13 @@ from spark_cluster.utils.spark_context import get_spark_context
 
 def spark_example_1(is_run_on_cluster: bool = False) -> None:
     """
-    Spark example nº 2 - Create and show DataFrame
+    Spark example nº 1 - Create and show DataFrame
     :param is_run_on_cluster: True if running aplication on the spark cluster, False otherwise
     :return: None
     """
-    spark = get_spark_context(app_name="pyspark_example_1", is_run_on_cluster=is_run_on_cluster)
+    spark = get_spark_context(
+        app_name="pyspark_example_1", is_run_on_cluster=is_run_on_cluster
+    )
     data = [
         ("James", "", "Smith", "1991-04-01", "M", 3000),
         ("Michael", "Rose", "", "2000-05-19", "M", 4000),
@@ -22,6 +24,7 @@ def spark_example_1(is_run_on_cluster: bool = False) -> None:
     df = spark.createDataFrame(data=data, schema=columns)
     df.show()
     spark.stop()
+
 
 if __name__ == "__main__":
     spark_example_1(is_run_on_cluster=True)
--- deploy/dev/spark/spark_cluster/utils/spark_context.py
+++ deploy/dev/spark/spark_cluster/utils/spark_context.py
@@ -4,12 +4,16 @@ from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from subprocess import check_output
 
-from spark_cluster.utils.spark_cluster_context_config import spark_cluster_context_config
+from spark_cluster.utils.spark_cluster_context_config import (
+    spark_cluster_context_config,
+)
 
 
-def get_spark_context(app_name: str, is_run_on_cluster: bool = False, context_log_level: str = "WARN") -> SparkSession:
+def get_spark_context(
+    app_name: str, is_run_on_cluster: bool = False, context_log_level: str = "WARN"
+) -> SparkSession:
     """
-    Get the spark context
+    Get the spark context and setting the log level
     :param app_name: Spark app name
     :param is_run_on_cluster: True if running aplication on the spark cluster, False otherwise
     :param context_log_level: Context log level
@@ -18,12 +22,17 @@ def get_spark_context(app_name: str, is_run_on_cluster: bool = False, context_lo
     spark_config = SparkConf()
     config_var_list = [("spark.app.name", app_name)]
     if is_run_on_cluster:
-        SPARK_DRIVER_HOST = check_output(["hostname", "-i"]).decode(encoding="utf-8").strip()
+        SPARK_DRIVER_HOST = (
+            check_output(["hostname", "-i"]).decode(encoding="utf-8").strip()
+        )
         # See: https://github.com/bitnami/bitnami-docker-spark/issues/18#issuecomment-701487655
         os.environ["SPARK_LOCAL_IP"] = SPARK_DRIVER_HOST
         config_var_list += [(k, v) for k, v in spark_cluster_context_config.items()] + [
             # spark.master = spark://<ip of master node>:<port of master node>
-            ("spark.master", os.environ.get("SPARK_MASTER_URL", "spark://spark-master:7077")),
+            (
+                "spark.master",
+                os.environ.get("SPARK_MASTER_URL", "spark://spark-master:7077"),
+            ),
             # spark.driver.host = IP of the driver (e.g. the machine / docker container from where we are running the
             # pyspark code)
             ("spark.driver.host", SPARK_DRIVER_HOST),
