34975caaa7f74dd43df472506134aefdb4dca6b4
victor-vila <victor.vila.navazo@gmail.com>
2022-11-21 14:33:10 +0100

    download Spark jobs from job run to disk

--- deploy/dev/spark/cloud/poc_spark_emr_serverless/analyse_emr_job.py
+++ deploy/dev/spark/cloud/poc_spark_emr_serverless/analyse_emr_job.py
@@ -1,9 +1,45 @@
+import os
 import botocore
 import boto3
+import gzip
+import shutil
 
+from path_interations import get_base_dir
 from deploy.dev.spark.cloud.poc_spark_emr_serverless.config import (
     poc_spark_emr_serverless_config as poc_config,
 )
+
+
+def download_s3_folder(job_run_id: str, job_name: str, local_dir: str = "deploy/dev/spark/cloud/poc_spark_emr_serverless/job_logs"):
+    """
+    Download the contents of a folder directory
+    Args:
+    bucket_name: the name of the s3 bucket
+    s3_folder: the folder path in the s3 bucket
+    local_dir: a relative or absolute directory path in the local file system
+    """
+    emr_app_id = poc_config["emr_serverless"]["app_id"]
+    s3 = boto3.resource("s3")
+    s3_bucket_name = poc_config["s3_bucket"]
+    bucket = s3.Bucket(name=s3_bucket_name)
+    common_log_path = f"{job_name}/applications/{emr_app_id}/jobs/{job_run_id}/"
+    s3_folder = f"logs/{common_log_path}"
+    target_base_dir = f"{get_base_dir()}/{local_dir}/{common_log_path}"
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        target = os.path.join(target_base_dir, os.path.relpath(obj.key, s3_folder))
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        bucket.download_file(obj.key, target)
+        # Unzip GZIP files as .txt files
+        if ".gz" in target:
+            with gzip.open(target, 'rb') as f_in:
+                with open(target.replace(".gz", ".txt"), 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            os.remove(target)
+    print(f"Job logs stored in the following folder: {target_base_dir}")
+    print("Check the SPARK_DRIVED/stdout.txt file in that folder for the STDOUT logs")
 
 
 def analyse_job_run(job_run_id: str) -> None:
@@ -50,6 +86,8 @@ def analyse_job_run(job_run_id: str) -> None:
                 )
         else:
             raise ValueError(f"EMR Serverless client error: {e}")
+    job_name = job_run_info["name"]
+    download_s3_folder(job_run_id=job_run_id, job_name=job_name)
 
 
 if __name__ == "__main__":
