53aef691fe37b7f896baf117d79eecb448a35337
victor-vila <victor.vila.navazo@gmail.com>
2023-02-21 16:30:40 +0100

    finish copying pulumi files + modifying env name if required

--- grizzly/deploy/grizzly_deploy_examples_cli.py
+++ grizzly/deploy/grizzly_deploy_examples_cli.py
@@ -1,10 +1,28 @@
 import typer
+from rich.prompt import Prompt
+from rich import print as rich_print
 
 from grizzly.deploy.spark.cloud.spark_emr_serverless.cli_example import SparkEmrServerlessCLIExample
 
 app = typer.Typer(
     rich_markup_mode="rich"
 )
+
+
+def explain_cli_format():
+    is_explain_legend = Prompt.ask(
+        prompt="[bold][blue]\nBefore we start the example, would you like an explanation of the formatting used "
+               "in the cli?",
+        choices=["y", "n"],
+        default="y"
+    )
+    if is_explain_legend == "y":
+        print(f"\n - Guidelines will appear in the standard format of your terminal (same as this line)")
+        rich_print(f" - [bold][blue]The inputs will be requested with prompts in blue")
+        rich_print(f" - [bold][magenta]The request choices will be in magenta within []")
+        rich_print(f" - [bold][cyan]The request defaults will be in cyan within (). If you directly type enter, "
+                   f"the default value will be used.\n")
+    print("Let's start the example!\n")
 
 
 @app.callback()
@@ -19,6 +37,7 @@ def emr_serverless():
     """
     Guided example to deploy Spark in AWS with EMR Serverless
     """
+    explain_cli_format()
     SparkEmrServerlessCLIExample().run_example()
 
 
--- grizzly/deploy/spark/cloud/spark_emr_serverless/cli_example.py
+++ grizzly/deploy/spark/cloud/spark_emr_serverless/cli_example.py
@@ -1,8 +1,10 @@
 import os
+import re
 import typer
 import inspect
-from distutils.dir_util import copy_tree
+import shutil
 from rich.prompt import Prompt
+from rich import print as rich_print
 
 import grizzly.iac_pulumi.aws.pulumi_projects.spark_emr_serverless
 
@@ -12,19 +14,72 @@ class SparkEmrServerlessCLIExample:
     def __init__(self):
         pass
 
-    def _copy_pulumi_files(self):
+    @staticmethod
+    def _recommend_pulumi_get_started_tutorial():
+        is_first_time = Prompt.ask(
+            prompt="[bold][blue]Is this the first time you use Pulumi to deploy AWS infrastructure?",
+            choices=["y", "n"],
+            default="y"
+        )
+        if is_first_time == 'y':
+            print(f"\nThen I recommend you to complete the following tutorial beforehand: "
+                  f"https://www.pulumi.com/docs/get-started/aws/begin/. After completing this tutorial, you should "
+                  f"have the prerequisites for this section, which are:")
+            print("- A Pulumi account with access to your AWS account.")
+            print("- Basic knowledge on how to create a Pulumi project and use basic pulumi commands to manage your "
+                  "infrastructure programmatically.")
+            Prompt.ask(
+                prompt="[bold][blue]\nPlease type enter when you are ready to continue.",
+            )
+
+    @staticmethod
+    def _get_environment_name() -> str:
+        print("\nWe will use a single environment for this example. We will name the Pulumi Stack as the environment "
+              "name.")
+        stack_name = Prompt.ask(
+            prompt="[bold][blue]\nPlease type the environment / stack name: ",
+            default="dev"
+        )
+        return stack_name
+
+    @staticmethod
+    def _copy_pulumi_files(stack_name: str):
         source_dir = os.path.dirname(inspect.getfile(grizzly.iac_pulumi.aws.pulumi_projects.spark_emr_serverless))
-        print(f"The Pulumi code will be copied from the directory: {source_dir}")
+        files_list = os.listdir(source_dir)
+        # Keep only "dev" environment file name
+        files_list = [file for file in files_list if
+                      not((file.startswith("Pulumi.") and file.endswith(".yaml") and "dev" not in file) and file != "Pulumi.yaml") and
+                      file != "__pycache__"]
+        print(f"\nThe Pulumi code will be copied from the directory: {source_dir}")
         dest_dir = Prompt.ask(
-            prompt="[blue]Please write down the target directory",
-            default="dir"
+            prompt="[bold][blue]\nPlease write down the target directory (should be empty or not existing yet)",
+            default="example_spark_emr_serverless"
         )
-        copy_tree(
-            src=source_dir,
-            dst=dest_dir
-        )
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        else:
+            if os.listdir(dest_dir):
+                raise ValueError("Destination directory is not empty")
+        new_file_list = []
+        for file in files_list:
+            if file == "Pulumi.dev.yaml":
+                new_file = file.replace("dev", stack_name)
+            else:
+                new_file = file
+            new_file_dir = f"{os.path.abspath(dest_dir)}/{new_file}"
+            shutil.copyfile(src=f"{source_dir}/{file}", dst=new_file_dir)
+            new_file_list.append(new_file)
+        print(f"\nThe folowing files have been created in {dest_dir}: {new_file_list}")
+
+    def _run_section_1(self):
+        rich_print("[bold][yellow]### SECTION 1 - Deploy the infrastructure ###\n")
+        print("In this section we will walk you through the steps to deploy the infrastructure as code using Pulumi.\n")
+        self._recommend_pulumi_get_started_tutorial()
+        stack_name = self._get_environment_name()
+        self._copy_pulumi_files(stack_name=stack_name)
+        # TODO - modify YAML file to fill the AWS account.
 
     def run_example(self):
-        self._copy_pulumi_files()
+        self._run_section_1()
 
 
