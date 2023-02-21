39081418757fc18e23c24f499f3951a5423a40c3
victor-vila <victor.vila.navazo@gmail.com>
2023-02-21 12:57:45 +0100

    start grizzly cli

---
+++ grizzly/grizzly_examples_cli.py
@@ -0,0 +1,37 @@
+import typer
+
+app = typer.Typer(
+    rich_markup_mode="rich"
+)
+
+
+@app.callback()
+def callback():
+    """
+    [bold][magenta]Grizzly guided examples to deploy PySpark code to cloud platforms using infrastructure as code (using Pulumi)
+    """
+
+
+@app.command(rich_help_panel="Spark - AWS")
+def emr_serverless():
+    """
+    Guided example to deploy Spark in AWS with EMR Serverless
+    """
+    print("Example coming soon!")
+
+
+@app.command(rich_help_panel="Spark - AWS")
+def emr_on_eks():
+    """
+    [COMING SOON] Guided example to deploy Spark in AWS with EMR on EKS
+    """
+    print("Example coming soon!")
+
+
+@app.command(rich_help_panel="Spark - GCP")
+def dataproc_serverless():
+    """
+    [NOT IMPLEMENTED YET] Guided example to deploy Spark in GCP with Dataproc Serverless
+    """
+    print("Not implemented yet.")
+
